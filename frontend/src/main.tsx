7f2519f0210b548f0c7cb1bb3d7484021c421b41
Ben Wozak <benwozak@gmail.com>
2025-07-22 20:11:26 -0600

    fixed dependancy compatability error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { HelmetProvider } from 'react-helmet-async'
 import { GlobalErrorBoundary } from '@/components/error'
 import './index.css'
 import App from './App.tsx'
@@ -26,14 +25,12 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GlobalErrorBoundary>
-      <HelmetProvider>
-        <QueryClientProvider client={queryClient}>
-          <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-            <App />
-            {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
-          </ClerkProvider>
-        </QueryClientProvider>
-      </HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+          <App />
+          {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+        </ClerkProvider>
+      </QueryClientProvider>
     </GlobalErrorBoundary>
   </StrictMode>,
 )
